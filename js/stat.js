beae08dded95758dd3c29d9763942f92d67e5788
Igor Beliakovski <nuevo-design@mail.ru>
2017-01-21 20:22:01 +0200

    Исправление ошибок

--- js/stat.js
+++ js/stat.js
@@ -16,7 +16,7 @@ window.renderStatistics = function (ctx, names, times) {
 
   var max = -1;
 
-  for(var i = 0 ; i < times.length; i++ ) {
+  for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
@@ -28,12 +28,12 @@ window.renderStatistics = function (ctx, names, times) {
   var step = histoHeight / max;
   var columnIndent = 90;
 
-  for (var i = 0; i < times.length; i++) {
+  for (i = 0; i < times.length; i++) {
     var name = names[i];
-    var time = times[i];
+        time = times[i];
     var height = step * time;
 
-    ctx.fillStyle = name == 'Вы' ? '#FF0000' : 'rgb(0, 0, ' + Math.round(80 + Math.random() * (255 - 80)) + ')';
+    ctx.fillStyle = name === 'Вы' ? '#FF0000' : 'rgb(0, 0, ' + Math.round(80 + Math.random() * (255 - 80)) + ')';
     ctx.fillRect(histoX + columnIndent * i, 245 - height, 40, height);
     ctx.fillStyle = '#000';
     ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 240 - height);
