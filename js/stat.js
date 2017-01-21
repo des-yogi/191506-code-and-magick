f77efdb6989dcf798cf1c04bea9141049c37c746
Igor Beliakovski <nuevo-design@mail.ru>
2017-01-21 18:11:40 +0200

    Внес изменения согласно заданию в ветку module2-task1

---
+++ js/stat.js
@@ -0,0 +1,42 @@
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 140, 40);
+  ctx.fillText('Список результатов:', 140, 60);
+
+  var max = -1;
+
+  for(var i = 0 ; i < times.length; i++ ) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
+    }
+  }
+
+  var histoHeight = 150;
+  var histoX = 140;
+  var step = histoHeight / max;
+  var columnIndent = 90;
+
+  for (var i = 0; i < times.length; i++) {
+    var name = names[i];
+    var time = times[i];
+    var height = step * time;
+
+    ctx.fillStyle = name == 'Вы' ? '#FF0000' : 'rgb(0, 0, ' + Math.round(80 + Math.random() * (255 - 80)) + ')';
+    ctx.fillRect(histoX + columnIndent * i, 245 - height, 40, height);
+    ctx.fillStyle = '#000';
+    ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 240 - height);
+    ctx.fillText(name, histoX + columnIndent * i, 260);
+  }
+};
